2b38e68b5cdd7b3ca6c76762d1a909683ddc7f9e
Dushyant Yadav <dushyant9309@gmail.com>
2019-02-24 20:22:06 +0530

    Add response storage functionality

--- base/models.py
+++ base/models.py
@@ -7,16 +7,25 @@ import os
 class Member(models.Model):
     user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
+    score = models.IntegerField(default=0)
 
 
 class Question(models.Model):
     questionkey = models.IntegerField(default=0, unique=False)
     content = models.TextField()
     answer = models.CharField(max_length=50)
-    answered_by = models.ManyToManyField(Member, related_name='questions_answered', blank=True)
+
+    attempted_by = models.ManyToManyField(Member, related_name='questions_attempted', blank=True)
+    review_by = models.ManyToManyField(Member, related_name='marked_for_review', blank=True)
+    answered_right = models.ManyToManyField(Member, related_name='ans_correctly', blank=True)
+    answered_wrong = models.ManyToManyField(Member, related_name="ans_wrongly", blank=True)
+    not_attempted_by = models.ManyToManyField(Member, related_name='not_attempted', blank=True)
+
     score_increment = models.IntegerField(default=10)
+    score_decrement = models.IntegerField(default=0)  #For negative marking
     is_image = models.BooleanField(default=False)
     is_mcq = models.BooleanField(default=False)
+
     def __str__(self):
         return self.content 
 
@@ -24,7 +33,13 @@ class Answer(models.Model):
     parent_question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE)
     content = models.TextField()
     is_correct = models.BooleanField(default=False)
+    key = models.IntegerField(default=0)
 
     def __str__(self):
         return self.content
-    
+
+class Response(models.Model):
+    member = models.ForeignKey(Member, related_name="full_response", on_delete=models.CASCADE, blank=True)
+    question = models.OneToOneField(Question, related_name="ques_response", on_delete=models.CASCADE, null=True)
+    answer_mcq = models.OneToOneField(Answer, related_name='ans_response', on_delete=models.CASCADE, null=True)
+    answer_text = models.CharField(max_length=250, blank=True)
--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from .models import Member, Question, Answer
+from .models import Member, Question, Answer, Response
 
 from django.shortcuts import render, redirect
 from django.contrib import messages
@@ -15,10 +15,40 @@ def instructions(request):
 def index(request):
     return render(request, 'base/main.html')
 
-# def check_answer(request):
-#     if request.method = 'POST':
-#         try:
-#             question
+
+def store_answer(request):
+    current_member = Member.objects.get(user=current_user)
+    if request.method == 'POST':
+        queskey = request.POST.get("queskey")
+        question = Question.objects.get(questionkey=queskey)
+        try:
+            anskey = request.POST.get("anskey")
+            answers = question.answers.all()
+            answer = answers.get(key=anskey)
+            a = Response(member=current_member, question=question, answer_mcq=answer)
+            a.save()            
+        except:
+            answer = request.POST.get("answer")
+            a = Response(member=current_member, question=question, answer_text=answer)
+            a.save() 
+
+def check_answers(request):
+    current_member = Member.objects.get(user=request.user)
+    full_response = current_member.full_response.all()
+    for response in full_response:
+        question = response.question
+        try:
+            if reponse.answer_mcq.is_correct:
+                current_member.score = current_member.score + question.score_increment
+            else:
+                current_member.score = current_member.score - question.score_decrement
+        except:
+            if response.answer_text == question.answer:
+                current_member.score = current_member.score + question.score_increment
+            else:
+                current_member.score = current_member.score - question.score_decrement
+
+
 
 def get_question(request, question_key):
     current_question = Question.objects.get(questionkey=question_key)
@@ -41,3 +71,50 @@ def get_question(request, question_key):
         return JsonResponse(data)
         
 
+
+#def check_answer(request):
+#     member = Member.objects.get(user=request.user)
+#     correct_flag = 0
+#     if request.method = 'POST':
+#         queskey = request.POST.get("queskey")
+#         question = Question.objects.get(questionkey=queskey)
+
+#         try:
+#             anskey = request.POST.get("anskey")
+#             answers = question.answers.all()
+#             answer = answers.get(key=anskey)
+#             if answer.is_correct:
+#                 correct_flag = 1
+#             else:
+#                 correct_flag = 2
+#         except:
+#             answer = request.POST.get("answer")
+#             if answer == question.answer:
+#                 correct_flag = 1
+#             else:
+#                 correct_flag = 2
+
+        
+#         if correct_flag == 1:
+#             if question in member.ans_correctly.all():
+#                 pass
+#             elif question in member.ans_wrongly.all():
+#                 member.score = member.score + question.score_increment + question.score_decrement
+#                 member.save()
+#             else:
+#                 member.score = member.score + question.score_increment
+#                 member.save()
+#         elif correct_flag == 2:
+#             if question in member.ans_correctly.all():
+#                 member.score = member.score - question.score_increment - question.score_decrement
+#                 member.save
+#             elif question in member.ans_wrongly.all():
+#                 pass
+#             else:
+#                 member.score = member.score - question.score_decrement
+#         else:
+#             pass
+        
+#         return HttpResponse(status=204)
+#     else:                                                   #What do I do with this??
+#         return HttpResponse("You weren't supposed to be here you know")
